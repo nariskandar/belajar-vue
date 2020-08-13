0a84e82f72feeb51326e009ceaacc3f9bac28709
nandariskan <silvercodee@gmail.com>
2020-08-13 23:15:56 +0700

    page index

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'myscript.js';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar - Vue JS</title>
+</head>
+<body>
+    <h3>Belajar</h3>
+    <div id="biodata">{{ nama }}</div>
+</body>
+</html>
