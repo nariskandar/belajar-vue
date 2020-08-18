bffbf720473d6394507170abb91b3684fce90acc
nandariskan <silvercodee@gmail.com>
2020-08-18 11:08:15 +0700

    computed

--- index.php
+++ index.php
@@ -8,12 +8,18 @@
 <body>
     <h3>Belajar</h3>
     
-    
     <div id="biodata">
         <span>{{nama}}</span>
         <br>
         <span>{{hobi}}</span>
     </div>
+
+    <div id="artikel">
+  <span v-bind:title="pesan">
+    Arahkan mouse Anda kesini dan tunggu dalam beberapa detik
+    untuk melihat judul pesan yang dinamis
+  </span>
+</div>
 
     
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
--- myscript.js
+++ myscript.js
@@ -1,7 +1,14 @@
-var latihan = new Vue ({
+var latihan1 = new Vue ({
     el: '#biodata',
     data: {
         nama: 'Helo Nama Saya Nandar !',
         hobi: 'Hobi bermain futsal '
     }
 })
+
+var latihan2 = new Vue({
+    el: '#artikel',
+    data: {
+      pesan: 'Anda memuat halaman ini pada jam ' + new Date().toLocaleString()
+    }
+  })
